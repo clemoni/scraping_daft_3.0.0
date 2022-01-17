5066fbf4e8c96e5d260f1e9e683ce87b34c59853
Clement Liscoet <clement.liscoet@gmail.com>
2022-01-17 23:06:07 +0000

    fix: sh script

--- kill_scraping.sh
+++ kill_scraping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ~/Development/Projects/scraping_daft/scraping_daft_3.0.0/ && \
-docker-compose down && \
-sleep 30s && \
-docker container ls && \
+cd ~/Development/Projects/scraping_daft/scraping_daft_3.0.0/ 
+docker-compose down 
+sleep 30
+docker container ls 
 test -z "$(docker ps -q 2>/dev/null)" && osascript -e 'quit app "Docker"'
 
--- run_scraping.sh
+++ run_scraping.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sleep 30 && \
-open -a Docker && \
-sleep 30s && \
-cd ~/Development/Projects/scraping_daft/scraping_daft_3.0.0/ && \
-sleep 30s && \
+open -a Docker 
+sleep 30 
+cd ~/Development/Projects/scraping_daft/scraping_daft_3.0.0/
+sleep 30
 docker-compose up -d 
